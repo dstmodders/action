c32f64964e437b245ee81f957e498664ab92eff1
Victor Popkov <victor@popkov.me>
2024-07-07 14:24:56 +0300

    Change jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['**/*.ts', '!**/coverage/**', '!**/node_modules/**'],
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/*.d.ts',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+  ],
   coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'json', 'clover'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: { '^@src/(.*)$': '<rootDir>/src/$1' },
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['(helpers|setup)\\.ts$'],
+  testRegex: ['(/__tests__/.*|(\\.|/)(test|spec))\\.ts$'],
 };
