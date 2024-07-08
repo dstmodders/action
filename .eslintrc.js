146642d404ba95960adf535bd6c037008881b941
Victor Popkov <victor@popkov.me>
2024-07-08 18:49:58 +0300

    Replace .eslintrc with .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    jest: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  ignorePatterns: ['lib/**/*.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+  },
+};
